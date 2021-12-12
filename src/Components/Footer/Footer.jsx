e96f0c923d5a7c4c1549941e0154beb685816973
Paweł Tuliński <pawel.tulinski@gmail.com>
2021-12-12 20:14:52 +0100

    Add footer styling with styled components

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,37 +1,126 @@
-import { NavLink } from 'react-router-dom';
-import './Footer.scss';
-import { Social } from '../Social/Social';
+import { NavLink } from "react-router-dom";
+import { Social } from "../Social/Social";
+import styled from "styled-components";
 
 export const Footer = () => (
-  <footer className="footer">
-    <div className="footer__left">
-      <div className="footer__logo">
-        LOGO
-      </div>
+  <StyledFooter>
+    <FooterSidebar>
+      <Logo>LOGO</Logo>
       <Social container="footer" />
-    </div>
-    <nav className="footer__nav">
-      <ul className="footer__nav-list">
-        <li className="footer__nav-item">
-          <NavLink className={({ isActive }) => isActive ? "footer__link footer__link--active" : "footer__link"} to="/">Home</NavLink>
-        </li>
-        <li className="footer__nav-item">
-          <NavLink className={({ isActive }) => isActive ? "footer__link footer__link--active" : "footer__link"} to="/shop">Shop</NavLink>
-        </li>
-        <li className="footer__nav-item">
-          <NavLink className={({ isActive }) => isActive ? "footer__link footer__link--active" : "footer__link"} to="/terms">Terms and Conditions</NavLink>
-        </li>
-        <li className="footer__nav-item">
-          <NavLink className={({ isActive }) => isActive ? "footer__link footer__link--active" : "footer__link"} to="/contact">Contact</NavLink>
-        </li>
-        <li className="footer__nav-item">
-          <NavLink className={({ isActive }) => isActive ? "footer__link footer__link--active" : "footer__link"} to="/cart">Cart</NavLink>
-        </li>
-        <li className="footer__nav-item">
-          <NavLink className={({ isActive }) => isActive ? "footer__link footer__link--active" : "footer__link"} to="/search">Search</NavLink>
-        </li>
-      </ul>
-    </nav>
-
-  </footer>
+    </FooterSidebar>
+    <FooterNav>
+      <FooterNavList>
+        <FooterListItem>
+          <StyledNavLink
+            to="/"
+          >
+            Home
+          </StyledNavLink>
+        </FooterListItem>
+        <FooterListItem>
+          <StyledNavLink to="/shop">Shop</StyledNavLink>
+        </FooterListItem>
+        <FooterListItem>
+          <StyledNavLink to="/terms">Terms and Conditions</StyledNavLink>
+        </FooterListItem>
+        <FooterListItem>
+          <StyledNavLink to="/contact">Contact</StyledNavLink>
+        </FooterListItem>
+        <FooterListItem>
+          <StyledNavLink to="/cart">Cart</StyledNavLink>
+        </FooterListItem>
+      </FooterNavList>
+    </FooterNav>
+  </StyledFooter>
 );
+
+const StyledFooter = styled.footer`
+   {
+    height: 100%;
+    width: 100%;
+    background-color: ${(props) => props.theme.backgroundBlack};
+    padding: 50px 12px;
+    display: flex;
+    @media (min-width: 576px) {
+        padding: 50px 12px;
+        align-items: center;
+        justify-content: space-around;
+  }
+`;
+
+const FooterSidebar = styled.div`
+   {
+    flex: 3;
+    text-align: center;
+    @media (min-width: 992px) {
+      flex: 1;
+    }
+  }
+`;
+const Logo = styled.div`
+   {
+    color: ${(props) => props.theme.textWhite};
+    font-size: 3rem;
+    margin-bottom: 20px;
+    @media (min-width: 576px) {
+      font-size: 3rem;
+    }
+  }
+`;
+
+const FooterNav = styled.nav`
+   {
+    display: none;
+    flex: 5;
+    @media (min-width: 576px) {
+       {
+        display: block;
+      }
+    }
+    @media (min-width: 992px) {
+      flex: 3;
+      text-align: center;
+    }
+  }
+`;
+
+const FooterNavList = styled.ul`
+   {
+    margin: 0;
+    list-style-type: none;
+    padding: 0;
+    columns: 2;
+    @media (min-width: 992px) {
+      columns: 5;
+      display: flex;
+      justify-content: center
+    }
+  }
+`;
+
+const FooterListItem = styled.li`
+   {
+    padding: 12px 20px;
+    font-size: 14px;
+    @media (min-width: 992px) {
+      &__nav-item {
+        display: inline-block;
+      }
+    }
+  }
+`;
+
+const StyledNavLink = styled(NavLink)`
+   {
+    position: relative;
+    text-decoration: none;
+    color: ${(props) =>
+      props.isActive ? props.theme.yellow : props.theme.textWhite};
+    text-transform: uppercase;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${(props) => props.theme.yellow};
+    }
+  }
+`;
